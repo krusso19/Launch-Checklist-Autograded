31ff5808036cf1d04d1aa8cf5af6873175162ed3
Katie Russo <krusso196@gmail.com>
2024-08-25 21:47:29 -0400

    planet data appears on website! the wrong way, i think

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-// Write your JavaScript code here!
+//Write your JavaScript code here!
+
+//const { formSubmission } = require("./scriptHelper");
+
+//like line 23 in script helper... what is testInput?
+//Q: which chapter tells me about how to modify text in a function, an example of this?
 
 window.addEventListener("load", function() {
+    let list = document.getElementById("faultyItems");
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    let form = document.querySelector("form"); //Help: query selector apendix in textbook!
+    form.addEventListener("submit", function(event) { //Help: Chp25 Validation with Input > get reference to inputs. event is necessary for next line
+        event.preventDefault();
+
+        let pilotInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotInput.value;
+
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let copilot = copilotInput.value;
+
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let fuelLevel = fuelLevelInput.value;
+
+        let cargoMassInput = document.querySelector("input[name=cargoMass]");
+        let cargoMass = cargoMassInput.value;
+
+        console.log("pilot",pilot,copilot,fuelLevel,cargoMass);
+
+        // alert(`
+        // Pilot Name: ${pilot}
+        // Copilot Name: ${copilot}
+        // Fuel Level: ${fuelLevel}
+        // Cargo Mass: ${cargoMass}
+        // `);
+
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+
     })
+
+    let listedPlanets = myFetch();
+    console.log(listedPlanets);
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
     
  });
+
+ //add destination you are supposed to send it to this thing. console log name of planet. selected planet.name. 
+ //inner HTML changes
+
+ //fuel = get element by id. this is how you change attributes to the elemnt.
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,35 +1,96 @@
 // Write your helper functions here!
 
-require('cross-fetch/polyfill');
+//require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
- }
+//     return `
+//             <div>
+//                  <h2>Mission Destination</h2>
+//                  <ol>
+//                      <li>Name: ${json[i].consolename}</li>
+//                      <li>Diameter: </li>
+//                      <li>Star: ${star}</li>
+//                      <li>Distance from Earth: </li>
+//                      <li>Number of Moons: </li>
+//                  </ol>
+//                  <img src=""> </img>
+//             </div>
+//         `
+ };
  
  function validateInput(testInput) {
-    
+    let result = "";
+    if (testInput === ""){
+        result = "Empty";
+    } else if (typeof(Number (testInput)) === Number){
+        result = "Is a Number";
+    } else { //isNaN(testInput)===true
+        result = "Not a Number"
+    }
+    return result; //alert(result) does work!
+
  }
- 
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
+ function formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoLevel) { //should be using validateInput but (testInput) is not defined
+    console.log("yay form submission!")
+
+    if (pilotName === ""){
+        alert("All fields are required")
+    } else {
+        list.style.visibility="visible"; //task does not say to make this visible
+        pilotStatus.innerHTML=`Pilot ${pilotName} is ready for lunch`;
+    }
+
+    if (copilotName === ""){
+        alert("All fields are required")
+    } else {
+        copilotStatus.innerHTML=`Copilot ${copilotName} is ready for lunch`;
+    }
+
+    if (fuelLevel < 10000){
+        list.style.visibility="visible";
+        fuelStatus.innerHTML="Not enough fuel for the journey";
+        launchStatus.innerHTML="Shuttle not ready for launch";
+        launchStatus.style.color="red";
+    }
+
+    if (cargoLevel > 10000){
+        list.style.visibility="visible";
+        cargoStatus.innerHTML="Too much mass for the shuttle to take off";
+        launchStatus.innerHTML="Shuttle not ready for launch";
+        launchStatus.style.color="red";
+    }
+
+    if ((cargoStatus.innerHTML === "Cargo mass low enough for launch") && (fuelStatus.innerHTML === "Fuel level high enough for launch") && (pilotName !== "") && (copilotName !== "")){
+        launchStatus.style.color="green";
+        launchStatus.innerHTML="Shuttle is ready for launch";
+    }
     
  }
  
  async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
+     let planetsReturned; //what is await? I don't actually get this, its just copied
+     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        response.json().then(function(json) {
+            const destination = document.getElementById("missionTarget");
+            let i = 0;
+            i = Math.floor(Math.random()*6);
+            console.log("78 script helper index",i);
+            destination.innerHTML = `
+            <div>
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: ${json[i].name}</li>
+                     <li>Diameter: ${json[i].diameter}</li>
+                     <li>Star: ${json[i].star}</li>
+                     <li>Distance from Earth: ${json[i].distance}</li>
+                     <li>Number of Moons: ${json[i].moons}</li>
+                 </ol>
+                 <img src="${json[i].image}"> </img>
+            </div>
+        `;            
+        })
+  });
  
      return planetsReturned;
  }
